a85e5e6e4f3213ab82a7f8a0966c9af83f88e145
Adrien Tétar <adri-from-59@hotmail.fr>
2015-06-02 21:19:56 +0200

    groupsView: add a stacking widget, expand controls and deploy the GUI elements

--- Lib/defconQt/groupsView.py
+++ Lib/defconQt/groupsView.py
@@ -3,14 +3,75 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+class GroupListWidget(QListWidget):
+    def __init__(self, groupNames, parent=None):
+        super(GroupListWidget, self).__init__(parent)
+        self.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.setSortingEnabled(True)
+        
+        for groupName in groupNames:
+            item = QListWidgetItem(groupName, self)
+            item.setFlags(item.flags() | Qt.ItemIsEditable)
+    
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Delete:
+            self.parent()._groupDelete()
+            event.accept()
+        else:
+            super(GroupListWidget, self).keyPressEvent(event)
+
+class GroupStackWidget(QWidget):
+    def __init__(self, font, glyphs=[], parent=None):
+        super(GroupStackWidget, self).__init__(parent)
+        self.ascender = font.info.ascender
+        self.glyphs = glyphs
+        self.maxWidth = max(glyph.width for glyph in self.glyphs) if len(self.glyphs) else 300
+        self.upm = font.info.unitsPerEm
+        self.padding = 10
+        self.alignRight = False
+    
+    def setGlyphs(self, glyphs):
+        self.glyphs = glyphs
+        self.maxWidth = max(glyph.width for glyph in self.glyphs) if len(self.glyphs) else 300
+        self.update()
+    
+    def sizeHint(self):
+        return QSize(self.maxWidth+2*self.padding, 400)
+    
+    def paintEvent(self, event):
+        # TODO: maybe use self.upm*(1+2*BufferHeight) for the denominator as in fontView
+        scale = self.height() / (self.upm*1.2)
+        x_offset = (self.width()-self.maxWidth*scale-self.padding*2)/2
+        if x_offset < 0:
+            scale *= 1+2*x_offset/(self.maxWidth*scale)
+            x_offset = 0
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.translate(self.padding, self.padding+(self.ascender*1.2)*scale)
+        painter.scale(scale, -scale)
+            
+        col = QColor(Qt.black)
+        col.setAlphaF(.2)
+        for glyph in self.glyphs:
+            if self.alignRight: dist = self.maxWidth - glyph.width
+            else: dist = 0
+            glyphPath = glyph.getRepresentation("defconQt.QPainterPath")
+            painter.save()
+            painter.translate(x_offset+dist, 0)
+            painter.fillPath(glyphPath, col)
+            painter.restore()
+
 class GroupCharacterWidget(CharacterWidget):
     def __init__(self, font, squareSize=56, scrollArea=None, parent=None):
         super(GroupCharacterWidget, self).__init__(font, squareSize, scrollArea, parent)
-        self.columns = 8
+        self.columns = 9
         self.scrollArea.setAcceptDrops(True)
         self.scrollArea.dragEnterEvent = self.pipeDragEnterEvent
         self.scrollArea.dropEvent = self.pipeDropEvent
+        self.resize(self.width(), 200)
     
+    # TODO: The standard QListWidget has scrollbar and does not need three times parent call.
+    # Find out how to handle that properly.
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Delete:
             self.parent().parent().parent().characterDeleteEvent(self._selection)
@@ -32,22 +93,22 @@ class GroupsWindow(QWidget):
         super(GroupsWindow, self).__init__(parent, Qt.Window)
         self.font = font
 
-        self.groupsList = QListWidget(self)
-        self.groupsList.setSelectionMode(QAbstractItemView.SingleSelection)
-        self.groupsList.setSortingEnabled(True)
-        for groupName in self.font.groups.keys():
-            item = QListWidgetItem(groupName, self.groupsList)
-            item.setFlags(item.flags() | Qt.ItemIsEditable)
+        self.groupsList = GroupListWidget(self.font.groups.keys(), self)
         self.groupsList.itemChanged.connect(self._groupRenamed)
+        # TODO: it seems grid layout extends the column regardless of this
+        self.groupsList.setMaximumWidth(4*56+1)
+        
+        self.stackWidget = GroupStackWidget(self.font, parent=self)
         
         self.scrollArea = QScrollArea(self)
         self.characterWidget = GroupCharacterWidget(self.font, scrollArea=self.scrollArea, parent=self)
         self.scrollArea.setWidget(self.characterWidget)
         self.groupsList.currentItemChanged.connect(self._groupChanged)
         
-        layout = QHBoxLayout(self)
-        layout.addWidget(self.groupsList)
-        layout.addWidget(self.scrollArea)
+        layout = QGridLayout(self)
+        layout.addWidget(self.groupsList, 0, 0, 5, 4)
+        layout.addWidget(self.stackWidget, 0, 4, 5, 4)
+        layout.addWidget(self.scrollArea, 5, 0, 4, 8)
         self.setLayout(layout)
         
         self.setWindowTitle("%s%s%s%s" % ("Groups window – ", self.font.info.familyName, " ", self.font.info.styleName))
@@ -57,6 +118,7 @@ class GroupsWindow(QWidget):
         glyphs = []
         for gName in self.font.groups[currentGroup]:
             glyphs.append(self.font[gName])
+        self.stackWidget.setGlyphs(glyphs)
         self.characterWidget.setGlyphs(glyphs)
     
     def _groupRenamed(self):
@@ -66,7 +128,12 @@ class GroupsWindow(QWidget):
         newKey = cur.text()
         self.font.groups[newKey] = self.font.groups[self.groups[index]]
         del self.font.groups[self.groups[index]]
-        #print(self.groupsList.currentItem().text())
+    
+    def _groupDelete(self):
+        cur = self.groupsList.currentItem()
+        del self.font.groups[cur.text()]
+        self.groupsList.takeItem(self.groupsList.currentRow())
+        self._groupChanged()
     
     def characterDeleteEvent(self, selection):
         currentGroup = self.groupsList.currentItem().text()
@@ -76,7 +143,7 @@ class GroupsWindow(QWidget):
         for key in sorted(selection, reverse=True):
             del currentGroupList[key]
         self.font.groups[currentGroup] = currentGroupList
-        self.characterWidget.update()
+        self._groupChanged()
     
     def characterDropEvent(self, event):
         currentGroup = self.groupsList.currentItem().text()
@@ -89,3 +156,11 @@ class GroupsWindow(QWidget):
             self.font.groups[currentGroup] = currentGroupList
         event.acceptProposedAction()
         self._groupChanged()
+        
+    def resizeEvent(self, event):
+        if self.isVisible():
+            margins = self.layout().contentsMargins()
+            width = event.size().width() - (margins.left() + margins.right())
+            self.characterWidget._sizeEvent(width)
+            self.stackWidget.update()
+        super(GroupsWindow, self).resizeEvent(event)
