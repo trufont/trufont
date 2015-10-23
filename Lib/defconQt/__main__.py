cc479ee8aa0c42f637e10a5fb71a210c43f7a989
Denis Jacquerye <moyogo@gmail.com>
2015-10-23 09:56:32 +0100

    defconQt/__main__.py: move code to main() and call when __name__ is “__main__”

--- Lib/defconQt/__main__.py
+++ Lib/defconQt/__main__.py
@@ -8,23 +8,24 @@ from PyQt5.QtGui import QIcon
 
 
 def main():
-    if __name__ == "__main__":
-        main()
 
-if len(sys.argv) > 1:
-    font = TFont(os.path.abspath(sys.argv[1]))
-else:
-    font = None
+    if len(sys.argv) > 1:
+        font = TFont(os.path.abspath(sys.argv[1]))
+    else:
+        font = None
 
-# from pycallgraph import PyCallGraph
-# from pycallgraph.output import GraphvizOutput
-representationFactories.registerAllFactories()
-# with PyCallGraph(output=GraphvizOutput()):
-app = Application(sys.argv)
-# TODO: http://stackoverflow.com/a/21330349/2037879
-app.setOrganizationName("A. Tétar & Co.")
-app.setApplicationName("TruFont")
-app.setWindowIcon(QIcon(":/resources/app.png"))
-window = MainWindow(font)
-window.show()
-sys.exit(app.exec_())
+    # from pycallgraph import PyCallGraph
+    # from pycallgraph.output import GraphvizOutput
+    representationFactories.registerAllFactories()
+    # with PyCallGraph(output=GraphvizOutput()):
+    app = Application(sys.argv)
+    # TODO: http://stackoverflow.com/a/21330349/2037879
+    app.setOrganizationName("A. Tétar & Co.")
+    app.setApplicationName("TruFont")
+    app.setWindowIcon(QIcon(":/resources/app.png"))
+    window = MainWindow(font)
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
