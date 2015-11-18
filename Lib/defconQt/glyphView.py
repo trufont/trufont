e9e1e81ed6d81c1795df256e5e5dfff54f383b34
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2015-11-17 22:27:21 -0200

    split out DisplayStyleSettings class into its own file

--- Lib/defconQt/glyphView.py
+++ Lib/defconQt/glyphView.py
@@ -29,33 +29,10 @@ from defconQt.addComponentDialog import AddComponentDialog
 from defconQt.addLayerDialog import AddLayerDialog
 from defconQt.anchorItem import AnchorItem
 from defconQt.componentItem import ComponentItem
-from defconQt.genericSettings import GenericSettings
+from defconQt.displayStyleSettings import DisplayStyleSettings
 from defconQt.gotoDialog import GotoDialog
 from defconQt.layerActionsDialog import LayerActionsDialog
 from defconQt.pixmapItem import PixmapItem
-
-class DisplayStyleSettings(GenericSettings):
-    _presets = (
-        ('Default', dict(
-            activeLayerOnTop=True, activeLayerFilled=True,
-            otherLayersFilled=False, activeLayerUseLayerColor=False,
-            otherLayerUseLayerColor=True, drawOtherLayers=True
-        )), ('Layer Fonts', dict(
-            activeLayerOnTop=False, activeLayerFilled=True,
-            otherLayersFilled=True, activeLayerUseLayerColor=True,
-            otherLayerUseLayerColor=True, drawOtherLayers=True
-        ))
-    )
-
-    _items = (
-        ('activeLayerOnTop', ('Active Layer on Top', True)),
-        ('activeLayerFilled', ('Active Layer Filled', True)),
-        ('activeLayerUseLayerColor', ('Active Layer use Custom Color', False)),
-        ('otherLayersFilled', ('Other Layers Filled', False)),
-        ('otherLayerUseLayerColor', ('Other Layers use Custom Color', True)),
-        ('drawOtherLayers', ('Show Other Layers', True))
-    )
-
 
 class MainGfxWindow(QMainWindow):
 
