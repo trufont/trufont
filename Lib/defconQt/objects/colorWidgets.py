5ace8546c2cec7d337bfafb475bc4419860e53be
Adrien Tétar <adri-from-59@hotmail.fr>
2016-01-04 16:22:12 +0100

    colorWidgets: add a TODO

--- Lib/defconQt/objects/colorWidgets.py
+++ Lib/defconQt/objects/colorWidgets.py
@@ -16,6 +16,7 @@ class ColorVignette(QWidget):
 
     colorChanged = pyqtSignal()
 
+    # TODO: consider painting on hover like QLineEdit
     def __init__(self, parent=None):
         super().__init__(parent)
         self._color = None
