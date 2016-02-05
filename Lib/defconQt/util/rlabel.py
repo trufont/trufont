8e9323d1dc0a6d32f26b1048b91878237f2ce272
Nikolaus Waxweiler <madigens@gmail.com>
2016-02-05 20:30:44 +0100

    Two spaces before a class definition.

--- Lib/defconQt/util/rlabel.py
+++ Lib/defconQt/util/rlabel.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QLabel
 
+
 # A QLabel for right-aligning text to edit boxes to cut down repetition.
 class RLabel(QLabel):
     def __init__(self, *args, **kwargs):
