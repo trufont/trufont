0125d809af4cb347e65233f051f1b0fdb88d3573
Yves <yduprat@gmail.com>
2018-11-06 23:11:39 +0100

    Update src/trufont/objects/undoredomgr.py

--- src/trufont/objects/undoredomgr.py
+++ src/trufont/objects/undoredomgr.py
@@ -13,40 +13,46 @@ import inspect
 # constants
 
 def sample_copypathsfromlayer(layer: "Layer"):
-    pass 
+    pass
 
 def sample_undoredo_align_fromcopy(layer: "Layer", old_paths: "Path", old_operation:str):
-    pass 
+    pass
 
+<<<<<<< HEAD
 params_undoredo = { 
                   'default':{'copy': (sample_copypathsfromlayer, 'layer'),
                                 'undo': (sample_undoredo_align_fromcopy, 'layer', 'old_datas', 'operation'), 
+=======
+params_undoredo = {
+                  '_alignDefault':{'copy': (sample_copypathsfromlayer, 'layer'),
+                                'undo': (sample_undoredo_align_fromcopy, 'layer', 'old_datas', 'operation'),
+>>>>>>> fe195648ca3c81d4d6882cd70c865bbd277a93cb
                                 'redo': (sample_undoredo_align_fromcopy, 'layer', 'new_datas', 'operation')
                                 },
                   'transform':{'copy': (sample_copypathsfromlayer, 'layer'),
-                                 'undo': (sample_undoredo_align_fromcopy, 'layer', 'old_datas', 'operation'), 
+                                 'undo': (sample_undoredo_align_fromcopy, 'layer', 'old_datas', 'operation'),
                                  'redo': (sample_undoredo_align_fromcopy, 'layer', 'new_datas', 'operation')
                                  }
-                 } 
+                 }
 
 DEFAULT_KEY="default"
 def decorate_undoredo(params_deco: Dict, func_expand_params: Callable):
-    """  decorate functions that modify a glyph 
-    make a save of a glyph (or a part) before the function call
-    call the function
-    make a save of glyph (or a part) after the function call 
-    append an action - the modification -- to the undoredo manager of the glyph 
+    """  decorate functions that modify a glyph:
+    1. make a save of a glyph (or a part) before the function call
+    2. call the function
+    3. make a save of glyph (or a part) after the function call
+    4. append an action - the modification -- to the undoredo manager of the glyph
     params_deco is a dict where:
-        key is the function name 
-        values is a dict with 3 entries as tuple where first item is: 
-            copy function to make the save    
-            undo function calling when undo 
+        key is the function name
+        values is a dict with 3 entries as tuple where first item is:
+            copy function to make the save
+            undo function calling when undo
             redo function calling when redo
         NOTE: The end of the tuple (as 'layer','old_datas', etc ...) is not useful for the moment
 
-    func_expand_params is a function that decompose from *args and **kwargs of the decorated function 
-    to expand to the param need by the copy, undo and redo functions - 
-    Actually 3 func_expand_params erturn 3 values: Layer, UndoRedoMgr and an a sring (name of current operation)  
+    func_expand_params is a function that decompose from *args and **kwargs of the decorated function
+    to expand to the param need by the copy, undo and redo functions -
+    Actually func_expand_params returns 3 values: Layer, UndoRedoMgr and an a sring (name of current operation)
 
     WARNING: It is the same format for all decorated function, at this time -  HAVE TO CHANGE SOON
     Note here that the Action class, the callback function give to undoredo mgr are partial function.
@@ -66,16 +72,16 @@ def decorate_undoredo(params_deco: Dict, func_expand_params: Callable):
             ret = None
             try:
                 sig = inspect.signature(fn)
-                logging.debug("DECORATE_UNDOREDO: signature{}".format(sig)) 
+                logging.debug("DECORATE_UNDOREDO: signature{}".format(sig))
 
                 #functions from dict
-                key = None 
+                key = None
                 if fn.__name__ in params_deco:
                     key = fn.__name__
                 elif DEFAULT_KEY in params_deco:
                     key = DEFAULT_KEY
 
-                logging.debug("DECORATE_UNDOREDO: key is {}".format(key)) 
+                logging.debug("DECORATE_UNDOREDO: key is {}".format(key))
 
                 if key:
                     params = params_deco[key]
@@ -83,12 +89,13 @@ def decorate_undoredo(params_deco: Dict, func_expand_params: Callable):
                     func_undo = params['undo'][0]
                     func_redo = params['redo'][0]
 
-                    logging.debug("DECORATE_UNDOREDO: func copy:{}".format(func_copy.__name__)) 
-                    logging.debug("DECORATE_UNDOREDO: func undo:{}".format(func_undo.__name__)) 
-                    logging.debug("DECORATE_UNDOREDO: func redo:{}".format(func_redo.__name__)) 
-                    logging.debug("DECORATE_UNDOREDO: func expand:{}".format(func_expand_params.__name__)) 
+                    logging.debug("DECORATE_UNDOREDO: func copy:{}".format(func_copy.__name__))
+                    logging.debug("DECORATE_UNDOREDO: func undo:{}".format(func_undo.__name__))
+                    logging.debug("DECORATE_UNDOREDO: func redo:{}".format(func_redo.__name__))
+                    logging.debug("DECORATE_UNDOREDO: func expand:{}".format(func_expand_params.__name__))
 
                     # expand params as layer, undoredomgr and operation
+<<<<<<< HEAD
                     logging.debug("DECORATE_UNDOREDO: expand params") 
                     obj, undoredo, operation = func_expand_params(*args)
                     logging.debug("DECORATE_UNDOREDO: operation is {} ob {}".format(operation, obj.__class__.__name__)) 
@@ -96,12 +103,22 @@ def decorate_undoredo(params_deco: Dict, func_expand_params: Callable):
                     #save datas before function call
                     logging.debug("DECORATE_UNDOREDO: copy before func") 
                     old_obj = func_copy(obj)
+=======
+                    logging.debug("DECORATE_UNDOREDO: expand params")
+                    layer, undoredo, operation = func_expand_params(*args)
+                    logging.debug("DECORATE_UNDOREDO: operation is {}".format(operation))
+
+                    #save datas before function call
+                    logging.debug("DECORATE_UNDOREDO: copy before func")
+                    old_datas = func_copy(layer)
+>>>>>>> fe195648ca3c81d4d6882cd70c865bbd277a93cb
 
                     # call func
-                    logging.debug("DECORATE_UNDOREDO: call func") 
+                    logging.debug("DECORATE_UNDOREDO: call func")
                     ret = fn(*args, **kwargs)
 
                     #save datas after function call
+<<<<<<< HEAD
                     logging.debug("DECORATE_UNDOREDO: copy after func") 
                     new_obj = func_copy(obj)
 
@@ -111,6 +128,17 @@ def decorate_undoredo(params_deco: Dict, func_expand_params: Callable):
                                     functools.partial(func_undo, obj, old_obj, operation), 
                                     functools.partial(func_redo, obj, new_obj, operation))
                     logging.debug("DECORATE_UNDOREDO: append action") 
+=======
+                    logging.debug("DECORATE_UNDOREDO: copy after func")
+                    new_datas = func_copy(layer)
+
+                    # append action to undoredomgr
+                    logging.debug("DECORATE_UNDOREDO: create action")
+                    action = Action(operation,
+                                    functools.partial(func_undo, layer, old_datas, operation),
+                                    functools.partial(func_redo, layer, new_datas, operation))
+                    logging.debug("DECORATE_UNDOREDO: append action")
+>>>>>>> fe195648ca3c81d4d6882cd70c865bbd277a93cb
                     undoredo.append_action(action)
 
                 else:
@@ -138,19 +166,19 @@ class Action(object):
 
 # @deco4class.decorator_classfunc('len_undo', 'len_redo', 'show_undo', 'show_redo')
 class UndoRedoMgr(object):
-    """ Manage memory and event abour undo/redo/append 
+    """ Manage memory and event abour undo/redo/append
     actions """
 
-    __slots__ = ("_logger", "_name", "_undo", "_redo", "_group",  
-                "callback_on_activated", "callback_after_undo", "callback_after_redo", 
+    __slots__ = ("_logger", "_name", "_undo", "_redo", "_group",
+                "callback_on_activated", "callback_after_undo", "callback_after_redo",
                 "callback_after_append")
 
     def __init__(self, name: str, logger: logging.Logger=None):
-        """ init for mgr: logger for messages 
+        """ init for mgr: logger for messages
         actions store in base system: stack """
 
         self._logger = logger
-        self._name = name 
+        self._name = name
         self._undo = []
         self._redo = []
         self._group = None
@@ -162,12 +190,12 @@ class UndoRedoMgr(object):
 		
     def set_callback_on_activated(self, callback, *args, **kwargs):
         if isinstance(callback, Callable):
-           self.callback_on_activated = functools.partial(callback, *args, *kwargs) 
+           self.callback_on_activated = functools.partial(callback, *args, *kwargs)
 
 
     def set_callback_after_undo(self, callback, *args, **kwargs):
         if isinstance(callback, Callable):
-           self.callback_after_undo = functools.partial(callback, *args, *kwargs) 
+           self.callback_after_undo = functools.partial(callback, *args, *kwargs)
 
 		
     def set_callback_after_redo(self, callback, *args, **kwargs):
@@ -189,41 +217,40 @@ class UndoRedoMgr(object):
         if self._redo:
             self._redo = []
         self._after_append_action()
-			
+
     def undo(self) -> Action:
         """ play undo, if undo stack is empty raises an exception (indexError)"""
         last_action = self._undo.pop()
         self._redo.append(last_action)
         self._after_undo()
-        return last_action  
+        return last_action
 
     def prepare_undo(self, name: str) -> bool:
         """ set a marker string to play later a group of undo """
         pass
 
     def perform_undo(self, to_name):
-        """ run undo from group of undo""" 
+        """ run undo from group of undo"""
         pass    	
 
     def redo(self) -> Action:
         """ play redo, if redo stack is empty raises an exception (indexError)"""
         last_action  = self._redo.pop()
         self._undo.append(last_action)
-        self._after_append_action()            
+        self._after_append_action()
         self._after_redo()
         return last_action
-    
+
     def can_undo(self) -> bool:
         return self._undo
 
-        
     def len_undo(self) -> int:
         """ show len of stack undo """
         return len(self._undo)
 			
 
     def can_redo(self) -> bool:
-        return self._redo 
+        return self._redo
 
 
     def len_redo(self) -> int:
@@ -233,12 +260,12 @@ class UndoRedoMgr(object):
 
     def show_undo(self) -> str:
         """ show undo contents """
-        return "{} -> {}".format(self._name, str(self._undo)) 
+        return "{} -> {}".format(self._name, str(self._undo))
 
 		
     def show_redo(self) -> str:
-        """ show redo contents """ 
-        return "{} -> {}".format(self._name, str(self._redo)) 
+        """ show redo contents """
+        return "{} -> {}".format(self._name, str(self._redo))
 
 
     def on_activated(self):
