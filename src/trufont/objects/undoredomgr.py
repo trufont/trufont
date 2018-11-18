6af900ad65e3b7aedac935ba9a62e9884305d589
Samuel Hornus <samuel.hornus@gmail.com>
2018-11-18 23:01:07 +0100

    fixed bug in layer_decorate_undoredo

--- src/trufont/objects/undoredomgr.py
+++ src/trufont/objects/undoredomgr.py
@@ -166,11 +166,11 @@ def layer_decorate_undoredo(func_get_layer: Callable,\
 
                 #save datas after function call
                 logging.debug("LAYER_DECORATE_UNDOREDO: copy after func") 
-                undo, redo = layer.endUndoGroup(NONAME)
+                undo_redo_and_the_rest = layer.endUndoGroup(NONAME)
 
                 # append action to undoredomgr
                 logging.debug("LAYER_DECORATE_UNDOREDO: create and append action on {}".format(op)) 
-                undoredo.append_action(Action(op, undo, redo))
+                undoredo.append_action(Action(op, *undo_redo_and_the_rest))
 
             except Exception as e:
                 logging.error("LAYER_DECORATE_UNDOREDO exception {}".format(str(e)))
