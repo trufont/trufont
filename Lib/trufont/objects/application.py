ec275466215b0b76bdc13cc33af23c32df758b3b
exer2k <exer2k@gmail.com>
2020-03-15 22:33:45 +0100

    Coordinates options now toggled off by default

--- Lib/trufont/objects/application.py
+++ Lib/trufont/objects/application.py
@@ -675,15 +675,22 @@ class Application(QApplication):
             (
                 Entries.View_Show_Points,
                 ("showGlyphOnCurvePoints", "showGlyphOffCurvePoints"),
+                True,
             ),
             (
                 Entries.View_Show_Coordinates_When_Selected,
                 ("showGlyphCoordinatesWhenSelected",),
+                False,
             ),
-            (Entries.View_Show_Point_Coordinates, ("showGlyphPointCoordinates",)),
+            (
+                Entries.View_Show_Point_Coordinates,
+                ("showGlyphPointCoordinates",),
+                False,
+            ),
             (
                 Entries.View_Show_Bezier_Handles_Coordinates,
                 ("showGlyphBezierHandlesCoordinates",),
+                False,
             ),
             (
                 Entries.View_Show_Metrics,
@@ -692,16 +699,18 @@ class Application(QApplication):
                     "showFontVerticalMetrics",
                     "showFontPostscriptBlues",
                 ),
+                True,
             ),
-            (Entries.View_Show_Images, ("showGlyphImage",)),
+            (Entries.View_Show_Images, ("showGlyphImage",), True,),
             (
                 Entries.View_Show_Guidelines,
                 ("showGlyphGuidelines", "showFontGuidelines"),
+                True,
             ),
         ]
-        for entry, attrs in elements:
+        for entry, attrs, checked in elements:
             action = menu.fetchAction(entry)
             action.setCheckable(True)
-            action.setChecked(drawingAttributes.get(attrs[0], True))
+            action.setChecked(drawingAttributes.get(attrs[0], checked))
             action.setData(attrs)
             action.triggered.connect(self.setDrawingAttribute)
